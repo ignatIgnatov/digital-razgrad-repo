cc8255b8ab77f117bdad26d0bc570e3f485a079d
Ignat Ignatov <ignatov_rs@abv.bg>
2023-06-08 15:36:04 +0300

    homework update

--- strings_ex_homework/src/Homework_01.java
+++ strings_ex_homework/src/Homework_01.java
@@ -1,6 +1,25 @@
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Homework_01 {
+
+    public static int numbersOfWordsInTextFile(String filename) throws IOException {
+        File file = new File("src/input.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        String line = "";
+        int count = 0;
+
+        while ((line = bufferedReader.readLine()) != null) {
+
+            String[] tempArray = line.split(" ");
+            count += tempArray.length;
+        }
+
+        bufferedReader.close();
+        return count;
+    }
 
     public static int numberOfWords(String text) {
         int count = 0;
@@ -11,13 +30,15 @@ public class Homework_01 {
         }
         return count;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a text:");
         String input = scanner.nextLine();
 
         System.out.println("The number of words is: " + numberOfWords(input));
+
+        System.out.println("The number of words in 'input.txt' file is: " + numbersOfWordsInTextFile("input.txt"));
 
     }
 }
